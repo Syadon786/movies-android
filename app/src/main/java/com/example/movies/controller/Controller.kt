c4008ddfdbb689f0568b92817e79a01d7ed00efb
Gábor Bóka <bokagabor4@gmail.com>
2022-07-31 19:42:33 +0200

    Controller get model movie functions

--- app/src/main/java/com/example/movies/controller/Controller.kt
+++ app/src/main/java/com/example/movies/controller/Controller.kt
@@ -9,6 +9,55 @@ class Controller(context : Context) {
     init {
         this.context = context
         this.model = Model(context)
-        this.model.moviesData
+    }
+
+    fun getMovieData(id : Int) : Model.Movie {
+        return this.model.moviesData[id]
+    }
+
+    fun getMovieTitle(id : Int) : String {
+        return this.model.moviesData[id].title
+    }
+
+    fun getMovieReleasedDate(id : Int) : String {
+        return this.model.moviesData[id].released
+    }
+
+    fun getMoviePlot(id : Int) : String {
+        return this.model.moviesData[id].plot
+    }
+
+    fun getMovieGenres(id : Int) : List<String> {
+        return this.model.moviesData[id].genre.replace(" ", "").split(",")
+    }
+
+    fun getMoviePlayTime(id : Int) : String {
+        return this.model.moviesData[id].playtime
+    }
+
+    fun getMovieDirector(id : Int) : String {
+        return this.model.moviesData[id].director
+    }
+
+    fun getMovieCost(id : Int) : String {
+        return this.model.moviesData[id].cost
+    }
+
+    fun getMovieProfit(id : Int) : String {
+        return this.model.moviesData[id].profit
+    }
+
+    fun getMovieCast(id : Int) : MutableList<Pair<String, String>> {
+        var castInfo : MutableList<Pair<String, String>> = mutableListOf()
+        val actors = this.model.moviesData[id].cast.split(',')
+        for(actor in actors) {
+            val temp = actor.split("#")
+            castInfo.add(Pair(temp[0], temp[1]))
+        }
+        return castInfo
+    }
+
+    fun getPosterName(id : Int) : String  {
+        return this.model.moviesData[id].poster
     }
 }
